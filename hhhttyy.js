28c581598786f57d7cd048dcf3042f5319423242
YanaMinina <yanaminina2006@gmail.com>
2026-04-20 19:43:25 +0500

    Add files via upload

---
+++ hhhttyy.js
@@ -0,0 +1,36 @@
+const IMAGE_SRCS = [
+
+  "https://github.com/YanaMinina/YanaMinina-/blob/main/гало.jpg?raw=true",
+
+  "https://github.com/YanaMinina/YanaMinina-/blob/main/сияние.jpg?raw=true",
+
+  "https://github.com/YanaMinina/YanaMinina-/blob/main/столбы.jpg?raw=true",
+
+  "https://github.com/YanaMinina/YanaMinina-/blob/main/перла.jpg?raw=true",
+
+  "https://github.com/YanaMinina/YanaMinina-/blob/main/лами.jpg?raw=true",
+
+  "https://github.com/YanaMinina/YanaMinina-/blob/main/сере.jpg?raw=true"
+
+];
+
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+
+const FACE_NAMES = [
+
+  "ГАЛО",
+
+  "СЕВЕРНОЕ СИЯНИЕ",
+
+  "СВЕТОВЫЕ СТОЛБЫ",
+
+  "ПЕРЛАМУТРОВЫЕ ОБЛАКА",
+
+  "ЛЕНТИКУЛЯРНЫЕ ОБЛАКА",
+
+  "СЕРЕБРИСТЫЕ ОБЛАКА"
+
+];
+
+
+
